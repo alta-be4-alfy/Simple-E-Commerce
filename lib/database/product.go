1538fe40818e07ae120c42a7f2e3a636bd630e01
alfynf <alfynurf36@gmail.com>
2021-11-17 23:46:24 +0700

    add feature get product by user id, update product, delete product

--- lib/database/product.go
+++ lib/database/product.go
@@ -37,3 +37,45 @@ func GetProduct(id int) (interface{}, error) {
 	}
 	return product, nil
 }
+
+// Fungsi untuk mendapatkan seluruh data product product tertentu berdasarkan id product
+func GetUserProducts(id int) (interface{}, error) {
+	var products []models.Products
+	query := config.DB.Find(&products, "users_id = ?", id)
+	if query.Error != nil {
+		return nil, query.Error
+	}
+	if query.RowsAffected == 0 {
+		return 0, query.Error
+	}
+	return products, nil
+}
+
+//Fungsi untuk memperbaharui data product berdasarkan id product
+func UpdateProduct(id int, updateProduct *models.Products) (interface{}, error) {
+	var product models.Products
+	query := config.DB.Find(&product, id)
+	if query.Error != nil {
+		return nil, query.Error
+	}
+	if query.RowsAffected == 0 {
+		return 0, query.Error
+	}
+	updateQuery := config.DB.Model(&product).Updates(updateProduct)
+	if updateQuery.Error != nil {
+		return nil, query.Error
+	}
+	return product, nil
+}
+
+// Fungsi untuk menghapus satu data product berdasarkan id product
+func DeleteProduct(id int) (interface{}, error) {
+	query := config.DB.Delete(&models.Products{}, id)
+	if query.Error != nil {
+		return nil, query.Error
+	}
+	if query.RowsAffected == 0 {
+		return 0, query.Error
+	}
+	return "deleted", nil
+}
--- routes/route.go
+++ routes/route.go
@@ -15,6 +15,7 @@ func New() *echo.Echo {
 	e.POST("/login", controllers.LoginUsersController) // jwt login
 	e.GET("/products", controllers.GetProductsController)
 	e.GET("/products/:id", controllers.GetProductController)
+	e.GET("/products/users/:id", controllers.GetUserProductController)
 
 	r := e.Group("/jwt")
 	r.Use(echoMid.JWT([]byte(constants.SECRET_JWT)))
@@ -22,5 +23,7 @@ func New() *echo.Echo {
 	r.DELETE("/users/:id", controllers.DeleteUserController) // jwt delete
 	r.PUT("/users/:id", controllers.UpdateUserController)    // jwt put
 	r.POST("/products", controllers.CreateProductController)
+	r.PUT("/products/:id", controllers.UpdateProductController)
+	r.DELETE("/products/:id", controllers.DeleteProductController)
 	return e
 }
