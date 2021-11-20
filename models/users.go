42d1808144317ba3ed81482f174c0657965a92f1
alfynf <alfynurf36@gmail.com>
2021-11-20 19:59:53 +0700

    delete shopping cart feature

--- models/users.go
+++ models/users.go
@@ -20,8 +20,3 @@ type UserLogin struct {
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
 }
-
-type UserLogin struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-}
